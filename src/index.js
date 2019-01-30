78a04c8e74146e668c06270ae5bcb12e88bb00a1
gco421 <graceco421@gmail.com>
2019-01-30 12:42:24 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Form = () => (
+  <React.Fragment>
+  <div class="col-md-6">
+  <h1>Welcome!</h1>
+  <h2>Register here</h2>
+  <form>
+  <label for="fn">First Name </label>
+  <input id="fn" name="fn" type="text"/>
+  <br/>
+  <label for="ln">Last Name </label>
+  <input id="ln" name="ln" type="text"/>
+  <br/>
+  <label for="email">Email </label>
+  <input id="email" name="email" type="text"/>
+  <br/>
+  <label for="password">Password: </label>
+  <input type="password" name="password"/>
+  <br/>
+  <label for="b ">Password Confirmation: </label>
+  <input type="password" name="password_confirmation"/>
+  <br/>
+  <input type="submit" class = "btn btn-primary" value="Submit"/>
+  </form> 
+  </div>
+ 
+ <div class="col-md-6">
+ <br/>
+ <br/>
+ <br/>
+ <h2>Login here</h2>
+ <form>
+ <label for="email">Email </label>
+ <input id="email" name="email" type="text"/>
+ <br/>
+ <label for="password">Password: </label>
+ <input type="password" name="password"/>
+ <br/>
+ <input type="submit" name="login" class = "btn btn-primary" value="Login"/>
+</form>
+</div>
+</React.Fragment>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+  // ========================================
+  
+  ReactDOM.render(
+    <Form />,
+    document.getElementById('root')
+  );
+  
